e00a1f5e02be14debbffa2fe0b8dc608d8c2c1f4
samereberlin <samereberlin@gmail.com>
2016-06-07 15:11:36 -0300

    Add dynamic fadeIn/fadeOut effect for page transitions.

--- www/WebApp.js
+++ www/WebApp.js
@@ -6,7 +6,7 @@ var page = null;
 document.addEventListener('DOMContentLoaded', load);
 
 function load() {
-	console.log('upsky.pagemanager.js: load()');
+	console.log('WebApp.js: load()');
 
 	// Setup page elements:
 	Array.prototype.forEach.call(document.body.children, function (node) {
@@ -22,27 +22,62 @@ function load() {
 }
 
 function updatePage(hashChangeEvent) {
-	console.log('upsky.pagemanager.js: updatePage(hashChangeEvent)... newURL = ' + hashChangeEvent.newURL + ', oldURL = ' + hashChangeEvent.oldURL);
+	console.log('WebApp.js: updatePage(hashChangeEvent)... newURL = ' + hashChangeEvent.newURL + ', oldURL = ' + hashChangeEvent.oldURL);
 
+	// Parse page URL (hash/search) data:
+	var newPageSearch = (window.location.hash.indexOf('?') >= 0)? window.location.hash.substring(window.location.hash.indexOf('?') + 1): null;
+	var newPageHash = (window.location.hash.length > 1)? window.location.hash.substring(1, (newPageSearch? window.location.hash.indexOf('?'): window.location.hash.length)): null;
+
+	// Query for the new requested page (pageHash):
 	var newPage = null;
-	if (window.location.hash.length > 0) {
+	if (newPageHash) {
 		for (var i = 0; i < pages.length; i++) {
-			if (pages[i].id == window.location.hash.substr(1)) {
+			if (pages[i].id === newPageHash) {
 				newPage = pages[i];
 				break;
 			}
 		}
 	}
 
+	// Parse the new requested parameters (pageSearch):
+	var newParams = {};
+	if (newPageSearch) {
+		var indexOfEqual = null;
+		newPageSearch = newPageSearch.split('&');
+		newPageSearch.forEach(function(param) {
+			indexOfEqual = param.indexOf('=');
+			if (indexOfEqual > 0) {
+				newParams[param.slice(0, indexOfEqual)] = param.slice(indexOfEqual + 1, param.length)
+			}
+		});
+	}
+
 	if (newPage) {
-		if (page) {
-			fadeOut(page, null, function() {
-				fadeIn(newPage, null, null);
-			});
-		} else fadeIn(newPage, null, null);
-		page = newPage;
+		if (newPage === page) {
+			// TODO: implement URL parameters updated engine.
+		} else {
+			switchPage(page, newPage, newParams['transition'], newParams['display']);
+			page = newPage;
+		}
 	} else if (page) window.history.back();
 	else window.location.replace(window.location.protocol + '//' + window.location.pathname + '#' + pages[0].id);
+}
+
+function switchPage(page, newPage, transition, display) {
+	switch (transition) {
+	case 'fade':
+		if (page) {
+			fadeOut(page, 'none', function() {
+				fadeIn(newPage, display, null);
+			});
+		} else fadeIn(newPage, display, null);
+		break;
+	default:
+		if (page) page.style.display = 'none';
+		newPage.style.display = display || 'block';
+		newPage.style.opacity = 1;
+		break;
+	}
 }
 
 function fadeOut(element, display, callback){
