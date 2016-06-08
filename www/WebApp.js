c329455f6ba23baacc62d2e17bf7ff64456a0840
samereberlin <samereberlin@gmail.com>
2016-06-08 19:19:41 -0300

    New Feature: Implement page onLoad/onShow/onHide callbacks.

--- www/WebApp.js
+++ www/WebApp.js
@@ -9,38 +9,45 @@ var WebAppClass = function() {
 	this.getFadeOn = function() {return isFadeOn;};
 	this.setFadeOn = function(state) {isFadeOn = state;};
 
-	var pageStack = {};
+	var pageStack = null;
 	var currentPage = null;
-	var defaultPage = null;
+
+	var defaultPageId = null;
+	this.getDefaultPageId = function() {return defaultPageId;};
+	this.setDefaultPageId = function(pageId) {defaultPageId = pageId;};
 
 	this.load = function() {
 		if (isDebugOn) console.log('WebApp.js: load()');
 
 		// Setup page elements:
-		Array.prototype.forEach.call(document.body.children, function (node) {
-			if (node.classList.contains('page') && node.id) {
-				node.styleDisplay = node.style.display;
-				node.style.opacity = node.styleOpacity;
-				node.style.display = 'none';
-				pageStack[node.id] = node;
-				if (!defaultPage) defaultPage = node.id;
-			}
-		});
+		if (!pageStack) {
+			pageStack = {};
+			Array.prototype.forEach.call(document.body.children, function (node) {
+				if (node.classList.contains('page') && node.id) {
+					node.styleDisplay = node.style.display;
+					node.style.opacity = node.styleOpacity;
+					node.style.display = 'none';
+					pageStack[node.id] = node;
+					if (!defaultPageId) defaultPageId = node.id;
+					if (typeof node['onLoad'] === 'function') node.onLoad();
+				}
+			});
 
-		// Setup page change listener:
-		window.addEventListener('hashchange', updatePage);
-		updatePage({'type': 'hashchange', 'newURL': null, 'oldURL': null}); // Required to set initial page.
+			// Setup page change listener:
+			window.addEventListener('hashchange', updatePage);
+			updatePage({'type': 'hashchange', 'newURL': null, 'oldURL': null}); // Required to set initial page.
+		}
 	};
 
 	function updatePage(hashChangeEvent) {
 		if (isDebugOn) console.log('WebApp.js: updatePage(hashChangeEvent)... newURL = ' + hashChangeEvent.newURL + ', oldURL = ' + hashChangeEvent.oldURL);
 
-		// Parse page URL (hash/search) data:
+		// Parse URL data:
 		var nextSearch = (window.location.hash.indexOf('?') >= 0)? window.location.hash.substring(window.location.hash.indexOf('?') + 1): null;
 		var nextHash = (window.location.hash.length > 1)? window.location.hash.substring(1, (nextSearch? window.location.hash.indexOf('?'): window.location.hash.length)): null;
 		var nextPage = (nextHash && pageStack[nextHash])? pageStack[nextHash]: null;
 
-		// Parse the requested parameters (nextSearch):
+		// Parse URL parameters:
 		var nextParams = {};
 		if (nextSearch) {
 			var indexOfEqual = null;
@@ -53,29 +60,44 @@ var WebAppClass = function() {
 			});
 		}
 
+		// Execute update action:
 		if (nextPage) {
 			if (nextPage === currentPage) {
 				// TODO: implement URL parameters updated engine.
 			} else {
-				if (isFadeOn) {
-					if (currentPage) {
-						fadeOut(currentPage, function() {
-							fadeIn(nextPage, null);
-						});
-					} else fadeIn(nextPage, null);
-				} else {
-					if (currentPage) currentPage.style.display = 'none';
-					nextPage.style.display = nextPage['styleDisplay'] || 'block';
-				}
+				switchElement(currentPage, nextPage);
 				currentPage = nextPage;
 			}
 		} else if (currentPage) window.history.back();
-		else window.location.replace(window.location.protocol + '//' + window.location.pathname + '#' + defaultPage);
+		else window.location.replace(window.location.protocol + '//' + window.location.pathname + '#' + defaultPageId);
+	}
+
+	function switchElement(currentElement, nextElement) {
+		if (isFadeOn) {
+			if (currentElement) {
+				fadeOut(currentElement, function() {
+					fadeIn(nextElement, null);
+				});
+			} else fadeIn(nextElement, null);
+		} else {
+			if (currentElement) hideElement(currentElement)
+			showElement(nextElement);
+		}
+	}
+
+	function showElement(element) {
+		element.style.display = element['styleDisplay'] || 'block';
+		if (typeof element['onShow'] === 'function') element.onShow();
+	}
+
+	function hideElement(element) {
+		if (typeof element['onHide'] === 'function') element.onHide();
+		element.style.display = 'none';
 	}
 
 	function fadeIn(element, callback) {
 		element.style.opacity = 0;
-		element.style.display = element['styleDisplay'] || 'block';
+		showElement(element);
 		(function fade() {
 			var val = parseFloat(element.style.opacity);
 			if ((val += .2) > (element['styleOpacity'] || 1)) {
@@ -91,7 +113,7 @@ var WebAppClass = function() {
 	function fadeOut(element, callback) {
 		(function fade() {
 			if ((element.style.opacity -= .2) < 0) {
-				element.style.display = 'none';
+				hideElement(element);
 				element.style.opacity = element['styleOpacity'] || 1;
 				if (typeof callback === 'function') callback();
 			} else {
@@ -100,20 +122,6 @@ var WebAppClass = function() {
 		})();
 	}
 
-	// Setup page elements:
-	Array.prototype.forEach.call(document.body.children, function (node) {
-		if (node.classList.contains('page') && node.id) {
-			node.styleDisplay = node.style.display;
-			node.style.opacity = node.styleOpacity;
-			node.style.display = 'none';
-			pageStack[node.id] = node;
-			if (!defaultPage) defaultPage = node.id;
-		}
-	});
-
-	// Setup page change listener:
-	window.addEventListener('hashchange', updatePage);
-	updatePage({'type': 'hashchange', 'newURL': null, 'oldURL': null}); // Required to set initial page.
-
 };
-document.addEventListener('DOMContentLoaded', function() {WebApp = new WebAppClass();});
+WebApp = new WebAppClass();
+document.addEventListener('DOMContentLoaded', function() {WebApp.load();});
