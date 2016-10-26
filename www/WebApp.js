08e2d325f10c8378995811e1a6788cf23030b2a6
Samer Eberlin <samereberlin@gmail.com>
2016-10-26 16:50:07 -0200

    Code review: modify WebApp.js to assure that WebApp.css has a higher priority than basic properties (basic styles must be inserted at the beginning of the head).

--- www/WebApp.js
+++ www/WebApp.js
@@ -522,7 +522,8 @@ var WebAppClass = function() {
 				'.modal {background-color: rgba(0, 0, 0, 0.5); position: fixed; top: 0; right: 0; bottom: 0; left: 0; overflow: auto; z-index: 3}' +
 				'.modal > * {background-color: white; border-radius: 0.3125em; box-shadow: 0 2px 12px rgba(0,0,0,0.6); margin: 10% auto 1em; max-width: 600px; width: 80%; overflow: hidden}'
 			;
-			document.getElementsByTagName('head')[0].appendChild(style);
+			var head = document.getElementsByTagName('head')[0];
+			head.insertBefore(style, head.firstChild);
 
 			// Setup application life cycle callbacks:
 			window.onblur = function() {pause();};
